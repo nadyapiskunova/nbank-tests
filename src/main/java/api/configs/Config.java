743e3e6c81181b7619dc06d734fd0bf28daff8c0
nadezhda.piskunova <nadejda.piskunova123@gmail.com>
2026-08-12 21:31:17 +0300

    test fail-2

--- src/main/java/api/configs/Config.java
+++ src/main/java/api/configs/Config.java
@@ -12,7 +12,7 @@ public class Config {
 
     private Config(){
         try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
+            if (input == null){
                 throw new RuntimeException("config.properties not found in resources");
             }
             properties.load(input);
@@ -21,10 +21,10 @@ public class Config {
         }
     }
 
-    public static String getProperty(String key) {
+    public static String getProperty(String key){
         // ПРИОРИТЕТ 1 - это системное свойство baseApiUrl = ..
         String systemValue = System.getProperty(key);
-        if (systemValue != null) {
+        if (systemValue != null){
             return systemValue;
         }
 
